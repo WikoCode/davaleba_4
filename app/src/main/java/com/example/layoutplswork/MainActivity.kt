4ba2b56ba3437417c570cb60af11fdbf8027627e
WikoCode <giorgitsik@gmail.com>
2023-11-03 02:10:33 +0400

    Initial commit

---
+++ app/src/main/java/com/example/layoutplswork/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.layoutplswork
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.caverock.androidsvg.SVG
+import com.caverock.androidsvg.SVGImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
